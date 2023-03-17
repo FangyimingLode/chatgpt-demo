58c1dc08ac78c3ff137d9eea20959dfd08b2faf9
Fangyiming <57992882+FangyimingLode@users.noreply.github.com>
2023-03-17 15:01:56 +0800

    apikey

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -9,7 +9,7 @@ const apiKey = import.meta.env.OPENAI_API_KEY
 const httpsProxy = import.meta.env.HTTPS_PROXY
 const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com').trim().replace(/\/$/,'')
 const sitePassword = import.meta.env.SITE_PASSWORD
-
+console.log(apiKey, 'apiKey')
 export const post: APIRoute = async (context) => {
   const body = await context.request.json()
   const { sign, time, messages, pass } = body
